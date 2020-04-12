3f4b82749c47a715c8b931b80075c327d68d8d83
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-04-12 10:21:39 -0500

    updates

--- notes.md
+++ notes.md
@@ -4,3 +4,19 @@ To compile: run `javac HelloWorld.java`
 To run: `java HelloWorld`
 
 You have to compile on every change to the programs
+
+In java variable have datatype label = value:
+`int studentAge = 15;`
+
+Data Types in Java provides a set of possible values
+Two main categories:
+##### Primitive Data Types: 
+- Boolean: true or false 
+- Int: represents a whole number
+- double: a decimal number
+- char: character single letter or symbol - use single quotes
+
+Reference Types:
+- String use double quotes - is built our of characters. Built using chars in a significant way.
+
+Print to the console: `System.out.println(studentAge);`
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Teresa";
+        String studentLastName = "Vasquez";
+
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+
+    }
+}
